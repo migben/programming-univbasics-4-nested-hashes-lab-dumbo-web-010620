17084a19ec3c91eb270551d5a9f5bf29299e34e1
Miguel Ben <itsmigben@gmail.com>
2019-12-09 18:33:50 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -67,7 +67,7 @@ def adding_matz
 # I'll proceed as told in the problem description
 
 # Not doing this:
-# Changing a hash by simply re-defining it is not the goal of this lab:
+# Changing a hash by simply re-defining it is not the goal of this lab.
 
 	programmer_hash = 
  		{
